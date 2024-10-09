765f9bf799f19543be8c65dca7e9d4f21f29f771
Natalia Furmanova <nfurmanova@tula.co>
2024-10-10 01:11:18 +0300

    feat: implement towelsort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    return (arguments.length===0||matrix.length===0)?[]:matrix.map((el, index)=>{
+        if(index%2!==0){
+          return el.reverse();
+        } 
+        return el;
+      }).flat();
 }
